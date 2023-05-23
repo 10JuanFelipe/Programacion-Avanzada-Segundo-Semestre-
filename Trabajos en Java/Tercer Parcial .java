71cf1d43866f5752db86c0d53eb73d07bb433b70
Juan Felipe González <124003588+10JuanFelipe@users.noreply.github.com>
2023-05-22 21:13:49 -0500

    Tercer Parcial .java

--- Trabajos en Java/Tercer Parcial .java
+++ Trabajos en Java/Tercer Parcial .java
@@ -1,5 +1,8 @@
+import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
+// Clase abstracta ConjuntoDeDatos
 abstract class ConjuntoDeDatos {
     protected String nombre;
     protected int tamaño;
@@ -12,22 +15,28 @@ abstract class ConjuntoDeDatos {
     public abstract String describir();
 }
 
+// Clase ConjuntoDeDatosTabular que hereda de ConjuntoDeDatos
 class ConjuntoDeDatosTabular extends ConjuntoDeDatos {
-    private int numeroDeFilas;
     private int numeroDeColumnas;
+    private int numeroDeFilas;
 
-    public ConjuntoDeDatosTabular(String nombre, int tamaño, int numeroDeFilas, int numeroDeColumnas) {
+    public ConjuntoDeDatosTabular(String nombre, int tamaño, int numeroDeColumnas, int numeroDeFilas) {
         super(nombre, tamaño);
-        this.numeroDeFilas = numeroDeFilas;
         this.numeroDeColumnas = numeroDeColumnas;
+        this.numeroDeFilas = numeroDeFilas;
     }
 
     @Override
     public String describir() {
-        return "Nombre: " + nombre + "\nTamaño: " + tamaño + "\nTipo: Tabular\nFilas: " + numeroDeFilas + "\nColumnas: " + numeroDeColumnas;
+        return "Nombre: " + nombre + "\n" +
+                "Tamaño: " + tamaño + "\n" +
+                "Tipo: Tabular" + "\n" +
+                "Filas: " + numeroDeFilas + "\n" +
+                "Columnas: " + numeroDeColumnas;
     }
 }
 
+// Clase ConjuntoDeDatosImagen que hereda de ConjuntoDeDatos
 class ConjuntoDeDatosImagen extends ConjuntoDeDatos {
     private int ancho;
     private int alto;
@@ -40,12 +49,17 @@ class ConjuntoDeDatosImagen extends ConjuntoDeDatos {
 
     @Override
     public String describir() {
-        return "Nombre: " + nombre + "\nTamaño: " + tamaño + "\nTipo: Imagen\nAncho: " + ancho + "\nAlto: " + alto;
+        return "Nombre: " + nombre + "\n" +
+                "Tamaño: " + tamaño + "\n" +
+                "Tipo: Imagen" + "\n" +
+                "Ancho: " + ancho + "\n" +
+                "Alto: " + alto;
     }
 }
 
+// Clase AnalizadorDeDatos
 class AnalizadorDeDatos {
-    private ArrayList<ConjuntoDeDatos> conjuntosDeDatos;
+    private List<ConjuntoDeDatos> conjuntosDeDatos;
 
     public AnalizadorDeDatos() {
         conjuntosDeDatos = new ArrayList<>();
@@ -56,38 +70,90 @@ class AnalizadorDeDatos {
     }
 
     public void eliminarConjuntoDeDatos(String nombre) {
-        for (int i = 0; i < conjuntosDeDatos.size(); i++) {
-            if (conjuntosDeDatos.get(i).nombre.equals(nombre)) {
-                conjuntosDeDatos.remove(i);
-                break;
-            }
-        }
+        conjuntosDeDatos.removeIf(conjunto -> conjunto.nombre.equals(nombre));
     }
 
-    public ArrayList<String> describirConjuntosDeDatos() {
-        ArrayList<String> descripciones = new ArrayList<>();
-        for (ConjuntoDeDatos conjuntoDeDatos : conjuntosDeDatos) {
-            descripciones.add(conjuntoDeDatos.describir());
+    public List<String> describirConjuntosDeDatos() {
+        List<String> descripciones = new ArrayList<>();
+        for (ConjuntoDeDatos conjunto : conjuntosDeDatos) {
+            descripciones.add(conjunto.describir());
         }
         return descripciones;
     }
 }
 
+// Clase ContadorConjuntosDeDatos
+class ContadorConjuntosDeDatos {
+
+    public void contarTipos(List<ConjuntoDeDatos> conjuntosDeDatos) {
+        int total = conjuntosDeDatos.size();
+        int contadorImagenes = 0;
+        int contadorTabulares = 0;
+
+        for (ConjuntoDeDatos conjunto : conjuntosDeDatos) {
+            if (conjunto instanceof ConjuntoDeDatosImagen) {
+                contadorImagenes++;
+            } else if (conjunto instanceof ConjuntoDeDatosTabular) {
+                contadorTabulares++;
+            }
+        }
+
+        double porcentajeImagenes = (double) contadorImagenes / total * 100;
+        double porcentajeTabulares = (double) contadorTabulares / total * 100;
+
+        System.out.printf("Cantidad de ConjuntoDeDatosImagen: %d (%.2f%%)\n", contadorImagenes, porcentajeImagenes);
+        System.out.printf("Cantidad de ConjuntoDeDatosTabular: %d (%.2f%%)\n", contadorTabulares, porcentajeTabulares);
+    }
+}
+
+// Función para deserializar una lista de objetos tipo ConjuntoDeDatos desde un archivo
+public static List<ConjuntoDeDatos> deserializarConjuntosDeDatos(String rutaArchivo) {
+    List<ConjuntoDeDatos> conjuntosDeDatos = new ArrayList<>();
+
+    try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(rutaArchivo))) {
+        conjuntosDeDatos = (List<ConjuntoDeDatos>) inputStream.readObject();
+    } catch (IOException | ClassNotFoundException e) {
+        System.out.println("Error al deserializar el archivo: " + e.getMessage());
+    }
+
+    return conjuntosDeDatos;
+}
+
 public class Main {
+
     public static void main(String[] args) {
-        ConjuntoDeDatosTabular conjunto1 = new ConjuntoDeDatosTabular("Datos de estudiantes", 1000, 200, 5);
-        ConjuntoDeDatosImagen conjunto2 = new ConjuntoDeDatosImagen("Imágenes de satélite", 2000, 1080, 720);
-        ConjuntoDeDatosImagen conjunto3 = new ConjuntoDeDatosImagen("Imágenes de satélite", 2000, 1080, 720);
-
         AnalizadorDeDatos analizador = new AnalizadorDeDatos();
-        analizador.añadirConjuntoDeDatos(conjunto1);
-        analizador.añadirConjuntoDeDatos(conjunto2);
-        analizador.añadirConjuntoDeDatos(conjunto3);
 
-        ArrayList<String> descripciones = analizador.describirConjuntosDeDatos();
+        // Crear instancias de ConjuntoDeDatosTabular y ConjuntoDeDatosImagen
+        ConjuntoDeDatos conjuntoTabular = new ConjuntoDeDatosTabular("Datos de estudiantes", 1000, 5, 200);
+        ConjuntoDeDatos conjuntoImagen1 = new ConjuntoDeDatosImagen("Imágenes de satélite", 2000, 1080, 720);
+        ConjuntoDeDatos conjuntoImagen2 = new ConjuntoDeDatosImagen("Imágenes de satélite", 2000, 1080, 720);
+
+        // Agregar conjuntos de datos al analizador
+        analizador.añadirConjuntoDeDatos(conjuntoTabular);
+        analizador.añadirConjuntoDeDatos(conjuntoImagen1);
+        analizador.añadirConjuntoDeDatos(conjuntoImagen2);
+
+        // Obtener descripciones de los conjuntos de datos
+        List<String> descripciones = analizador.describirConjuntosDeDatos();
+
+        // Imprimir descripciones
         for (String descripcion : descripciones) {
             System.out.println(descripcion);
-            System.out.println("--------------------------");
+            System.out.println("------------------------------");
+        }
+
+        // Contar la cantidad de ConjuntoDeDatosImagen y ConjuntoDeDatosTabular
+        ContadorConjuntosDeDatos contador = new ContadorConjuntosDeDatos();
+        contador.contarTipos(analizador.conjuntosDeDatos);
+
+        // Deserializar conjuntos de datos desde un archivo
+        String rutaArchivo = "conjuntos_de_datos.ser";
+        List<ConjuntoDeDatos> conjuntosDeserializados = deserializarConjuntosDeDatos(rutaArchivo);
+        System.out.println("\nConjuntos de datos deserializados:");
+        for (ConjuntoDeDatos conjunto : conjuntosDeserializados) {
+            System.out.println(conjunto.describir());
+            System.out.println("------------------------------");
         }
     }
 }
